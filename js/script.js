94fec89237f4ef52d281f5a71feb1593d5814243
EnricoCiccolini <piroroma90@gmail.com>
2025-03-06 14:26:32 +0100

    feat: add number list random

--- js/script.js
+++ js/script.js
@@ -4,13 +4,25 @@
 const countdownElement = document.getElementById('countdown')
 console.log(countdownElement)
 
+const numbersListElement = document.getElementById('numbers-list')
+console.log (numbersListElement)
+
+
+// numeri randomici //
+let number1 = randomNymber()
+let number2 = randomNymber()
+let number3 = randomNymber()
+let number4 = randomNymber()
+let number5 = randomNymber()
 
 
 
-
-
-
-
+numbersListElement.innerHTML = `
+<li>${number1}</li>
+<li>${number2}</li>
+<li>${number3}</li>
+<li>${number4}</li>
+<li>${number5}</li>`
 
 
 
@@ -35,7 +47,7 @@ function start() {
     countdownElement.innerHTML = count
     count --
     if(count === 0){
-        countdownElement.innerHTML = count('via')
+        
         clearInterval(timer)
 
     }
@@ -43,4 +55,10 @@ function start() {
 }
 
 //random//
-function randomNymber (){}
+
+function randomNymber (){
+   return Math.floor(Math.random() * 50) + 1
+}
+
+
+
