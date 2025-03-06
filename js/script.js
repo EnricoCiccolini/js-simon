29558927a4af6219898482c8d6aa21f0f25f6789
EnricoCiccolini <piroroma90@gmail.com>
2025-03-06 16:44:32 +0100

    fixed : message color and center

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const countdownElement = document.getElementById('countdown')
 console.log(countdownElement)
 
 const numbersListElement = document.getElementById('numbers-list')
-console.log (numbersListElement)
+console.log(numbersListElement)
 
 const answersFormElement = document.getElementById('answers-form')
 console.log(answersFormElement)
@@ -18,7 +18,7 @@ console.log(messageElement)
 
 
 //numeri del form
-const num1Element= document.getElementById('num1')
+const num1Element = document.getElementById('num1')
 console.log(num1Element)
 const num1ElemntValue = num1Element.value
 console.log(num1ElemntValue)
@@ -45,23 +45,29 @@ numbersListElement.innerHTML = `
 
 answersFormElement.addEventListener('submit', function (event) {
     event.preventDefault()
-   const formControlElement = document.querySelectorAll('.form-control')
-   console.log( formControlElement)
-   
-   let solved = 0
-    
-   for(let i  = 0 ; i < formControlElement.length ; i ++  ){
-     let control = formControlElement[i].value
-     control = parseInt(control)
-     if (control === number1 || control === number2 || control === number3|| control === number4|| control === number5)   
-        solved ++
-    
-   
+    const formControlElement = document.querySelectorAll('.form-control')
+    console.log(formControlElement)
+
+    let solved = 0
+
+    for (let i = 0; i < formControlElement.length; i++) {
+        let control = formControlElement[i].value
+        control = parseInt(control)
+        if (control === number1 || control === number2 || control === number3 || control === number4 || control === number5)
+            solved++
+
+
     }
+    if (solved === 0) {
+        messageElement.innerHTML = `Mi spiace non hai indovinato nessun numero`
+    } else if (solved === 1) {
+        messageElement.innerHTML = `complimenti hai indovinato ${solved} numero`
+        messageElement.className = 'text-success text-center'
+    } else if( solved >1){
 
-
-    messageElement.innerHTML =`complimenti hai indovinato ${solved} numeri`
-
+        messageElement.innerHTML = `complimenti hai indovinato ${solved} numeri`
+        messageElement.className = 'text-success text-center'
+    }
 })
 
 
@@ -73,26 +79,26 @@ answersFormElement.addEventListener('submit', function (event) {
 
 
 //contatore//
-let count = 1
-let timer = setInterval(start,1000 )
+let count = 10
+let timer = setInterval(start, 1000)
 
 function start() {
     countdownElement.innerHTML = count
-    count --
-    if(count === 0){
+    count--
+    if (count === 0) {
         countdownElement.className = 'd-none'
         numbersListElement.className = 'd-none'
         answersFormElement.className = 'd-block'
         clearInterval(timer)
 
     }
-    
+
 }
 
 //random//
 
-function randomNumber (){
-   return Math.floor(Math.random() * 50) + 1
+function randomNumber() {
+    return Math.floor(Math.random() * 50) + 1
 }
 
 
